195ba3710a75345cbfc22de27a172ef03d1bb33d
Athul <125321052+athulrs177@users.noreply.github.com>
2023-12-20 17:03:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CPA_3D',
+    name='CPA_parallel',
     version='0.1.0',
     url="https://github.com/athulrs177/CPA",
     author="Athul Rasheeda Satheesh",
     author_email="athulrs177@gmail.com",
-    packages=["CPA_3D"],
+    packages=["CPA_parallel"],
     install_requires=[
         'numpy',
         'xarray',
